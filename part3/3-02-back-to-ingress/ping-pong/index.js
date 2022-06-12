4e9bf2822d8d0f4f8baa9f70b619c8ecc292af53
Johannes Palvanen <palvanenj@gmail.com>
2022-06-12 10:31:05 +0300

    Add 3.02

---
+++ part3/3-02-back-to-ingress/ping-pong/index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const { PrismaClient } = require("@prisma/client");
+
+const app = express();
+const port = process.env.PORT || 3000;
+const prisma = new PrismaClient();
+
+const handleServerStarted = () => {
+  console.log(`Server listening on port ${port}!`);
+};
+
+const handlePong = async (req, res) => {
+  const currentPongCount = await prisma.pong.count();
+  await prisma.pong.create({ data: {} });
+  res.json(currentPongCount);
+};
+
+app.use(express.json());
+app.get("/", (req, res) => res.send("PING PONG!"));
+app.get("/pingpong", handlePong);
+
+app.listen(port, handleServerStarted);
