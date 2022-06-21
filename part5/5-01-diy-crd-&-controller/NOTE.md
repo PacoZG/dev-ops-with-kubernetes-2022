3f1718dd2177dee396effc520264c48002ea78a0
Johannes Palvanen <palvanenj@gmail.com>
2022-06-21 15:29:53 +0300

    Add 5.01

---
+++ part5/5-01-diy-crd-&-controller/NOTE.md
@@ -0,0 +1,1 @@
+Once all is applied, the app will serve the copied website from `/` port 80.
---
+++ part5/5-01-diy-crd-&-controller/app/index.js
@@ -0,0 +1,75 @@
+const k8s = require("@kubernetes/client-node");
+const axios = require("axios").default;
+const express = require("express");
+
+const app = express();
+const kc = new k8s.KubeConfig();
+const port = process.env.PORT || 3000;
+
+kc.loadFromDefault();
+
+let watchReq = null;
+let latestPage = `
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <Title>No content yet</Title>
+  </head>
+  <body>
+    <h1>No site added yet, create a dummysite resource!</h1>
+  </body>
+</html>
+`;
+const watch = new k8s.Watch(kc);
+
+async function fetchAndSaveSite(url) {
+  const response = await axios.get(url);
+  latestPage = response.data;
+}
+
+watch
+  .watch(
+    "/apis/stable.dwk/v1/dummysites",
+    // optional query parameters can go here.
+    {
+      allowWatchBookmarks: true,
+    },
+    // callback is called for each received object.
+    (type, apiObj, watchObj) => {
+      if (type === "ADDED") {
+        console.log("new object:");
+        const url = apiObj?.spec?.website_url;
+        if (!url || typeof url !== "string") {
+          return;
+        }
+
+        fetchAndSaveSite(url);
+      } else if (type === "MODIFIED") {
+        console.log("changed object:");
+      } else if (type === "DELETED") {
+        console.log("deleted object:");
+      } else if (type === "BOOKMARK") {
+        console.log(`bookmark: ${watchObj.metadata.resourceVersion}`);
+      } else {
+        console.log("unknown type: " + type);
+      }
+      console.log(apiObj);
+    },
+    // done callback is called if the watch terminates normally
+    (err) => {
+      console.log(err);
+    }
+  )
+  .then((req) => {
+    // watch returns a request object which you can use to abort the watch.
+    watchReq = req;
+  });
+
+process.on("SIGTERM", () => {
+  if (watchReq !== null) {
+    watchReq.abort();
+  }
+});
+
+app.get("/", (req, res) => res.send(latestPage));
+app.listen(port, () => console.log(`Server listening on ${port}`));
